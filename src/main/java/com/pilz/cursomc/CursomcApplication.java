b7cad27fb865c3fa4bc128422274dada93e55b9d
Leandro Pilz <leandro.pilzz@gmail.com>
2019-04-08 17:13:23 -0300

    Cliente, TipoCliente, telefones e endereços.

--- src/main/java/com/pilz/cursomc/CursomcApplication.java
+++ src/main/java/com/pilz/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.pilz.cursomc.domain.Categoria;
 import com.pilz.cursomc.domain.Cidade;
+import com.pilz.cursomc.domain.Cliente;
+import com.pilz.cursomc.domain.Endereco;
 import com.pilz.cursomc.domain.Estado;
 import com.pilz.cursomc.domain.Produto;
+import com.pilz.cursomc.domain.enums.TipoCliente;
 import com.pilz.cursomc.repositories.CategoriaRepository;
 import com.pilz.cursomc.repositories.CidadeRepository;
+import com.pilz.cursomc.repositories.ClienteRepository;
+import com.pilz.cursomc.repositories.EnderecoRepository;
 import com.pilz.cursomc.repositories.EstadoRepository;
 import com.pilz.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -35,35 +44,45 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		// Categorias e produtos.
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
 
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		//
 
+		// Estados e Cidades.
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
-
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		//
+
+		// Clientes e Endereços.
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "43726999051", TipoCliente.PESSOA_FISICA);
+		cli1.getTelefones().addAll(Arrays.asList("999999999", "888888888"));
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "93950000", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "93950000", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		//
 
 	}
 }
---
+++ src/main/java/com/pilz/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,35 @@
+package com.pilz.cursomc.domain.enums;
+
+public enum TipoCliente {
+	PESSOA_FISICA(1, "Pessoa Física"), PESSOA_JURICIDA(2, "Pessoa Jurídica");
+
+	private int cod;
+	private String descricao;
+
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public static TipoCliente toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+
+		for (TipoCliente tp : TipoCliente.values()) {
+			if (cod.equals(tp.getCod())) {
+				return tp;
+			}
+		}
+
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
